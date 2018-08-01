60c829475c7503d30c3ca0f6b931772842920363
Haigang Liu <haigang@email.sc.edu>
2018-08-01 11:52:13 -0400

    now this function also works for flood data

--- RangeSelector.py
+++ RangeSelector.py
@@ -12,22 +12,26 @@ class RangeSelector:
     see the detaile below.
 
     Args:
-        monthly_rainfall_df (pandas dataframe): a pandas dataframe with
-        columns at least these: 'STATION', 'YEAR', 'MONTH', 'LONGITUDE', 'LATITUDE', 'PRCP', 'SST'
+        monthly_df (pandas dataframe): a pandas dataframe with
+        columns at least these: self.unique_column_name, 'YEAR', 'MONTH', 'LONGITUDE', 'LATITUDE', 'PRCP', 'SST'
 
         in_date (string): the starting year and month eg. '2010-10'
         out_date (string): the ending year and month eg. '2010-10'
     '''
 
-    def __init__(self, monthly_rainfall_df, in_date, out_date):
+    def __init__(self, monthly_df, in_date, out_date, unique_column_name = 'STATION', response_var = 'PRCP'):
 
-        self.monthly_rainfall_df = monthly_rainfall_df
+        self.unique_column_name = unique_column_name
+        self.monthly_df = monthly_df
+
+        self.response_var = response_var
         self.in_date = in_date
         self.out_date = out_date
-        self.monthly_rainfall_df.DATE = pd.to_datetime(self.monthly_rainfall_df.DATE)
-        self._select_complete_records_with_given_range()
 
-    def _select_complete_records_with_given_range(self):
+        self.monthly_df.DATE = pd.to_datetime(self.monthly_df.DATE)
+        self._select_df_within_given_range()
+
+    def _select_df_within_given_range(self):
 
         starting = self.in_date; ending = self.out_date
         print('both begining month and ending month will be included')
@@ -40,16 +44,16 @@ class RangeSelector:
         n_years = abs(delta.years)
         self.month_count = n_months + n_years*12
 
-        df_w_quanlified_dates = self.monthly_rainfall_df[(self.monthly_rainfall_df.DATE >= starting) \
-                                           & (self.monthly_rainfall_df.DATE <= ending)]
-        stations_w_complete_records_ = df_w_quanlified_dates.groupby('STATION').count().PRCP == self.month_count
+        df_w_quanlified_dates = self.monthly_df[(self.monthly_df.DATE >= starting) \
+                                           & (self.monthly_df.DATE <= ending)]
+        stations_w_complete_records_ = df_w_quanlified_dates.groupby(self.unique_column_name).count()[self.response_var] == self.month_count
         stations_w_complete_records = list(stations_w_complete_records_.index[stations_w_complete_records_.values])
-        self.complete_records_wo_sst = df_w_quanlified_dates[df_w_quanlified_dates.STATION.isin(stations_w_complete_records)]
+        self.complete_records_wo_sst = df_w_quanlified_dates[df_w_quanlified_dates[self.unique_column_name].isin(stations_w_complete_records)]
 
     def merge_with_sst(self, nc_data_dir, nc_mask_dir):
 
         cal = SSTcalculator(nc_data_dir = nc_data_dir, nc_mask_dir = nc_mask_dir)
-        lats_and_lons = self.complete_records_wo_sst.groupby('STATION').first().reset_index()[['STATION', 'LONGITUDE', 'LATITUDE']]
+        lats_and_lons = self.complete_records_wo_sst.groupby(self.unique_column_name).first().reset_index()[[self.unique_column_name, 'LONGITUDE', 'LATITUDE']]
         list_of_forms = cal.look_up_engine(starting_date = self.in_date,
                                            end_date= self.out_date,
                                            spatial_points = lats_and_lons)
@@ -57,62 +61,74 @@ class RangeSelector:
         sst_forms = pd.concat(list_of_forms, axis = 0) #stack up (by row)
         sst_forms = sst_forms.drop(['LONGITUDE','LATITUDE'], axis =1) #drop a column
 
-        temp1 = self.complete_records_wo_sst.set_index(['STATION', 'YEAR', 'MONTH'])
-        temp2 = sst_forms.set_index(['STATION', 'YEAR', 'MONTH'])
+        temp1 = self.complete_records_wo_sst.set_index([self.unique_column_name, 'YEAR', 'MONTH'])
+        temp2 = sst_forms.set_index([self.unique_column_name, 'YEAR', 'MONTH'])
 
         merged_table = pd.merge(temp1, temp2, left_index= True, right_index= True).reset_index()
-
-        try:
-            merged_table.drop(['Unnamed: 0'], axis =1, inplace = True)
-        except KeyError:
-            pass
 
         self.merged_table = merged_table
         return merged_table
 
-    def flip_to_flat_and_wide(self, vars_to_retain = None):
+    def flip_to_flat_and_wide(self, vars_to_retain):
 
         if vars_to_retain is None:
             vars_to_retain = ['PRCP', 'SST','RANGE_HIGH','RANGE_OVERALL', 'RANGE_LOW','TMIN', 'TMAX']
 
         column_name = []
+
         for var in vars_to_retain:
             for i in range(self.month_count):
                 column_name.append(var + str(i + 1))
 
-        location_info = self.merged_table[['STATION', 'LATITUDE', 'LONGITUDE']]
-        unique_locations = location_info.groupby('STATION').first().reset_index().copy()
-
         try:
-            df_reindexed = self.merged_table.set_index(['STATION', 'DATE'])
+            location_info = self.merged_table[[self.unique_column_name, 'LATITUDE', 'LONGITUDE']]
+            df_reindexed = self.merged_table.set_index([self.unique_column_name, 'DATE'])
 
-        except:
-            raise AttributeError('Need to first call merge_with_sst()')
+        except AttributeError:
+            print('skipped the step of calculating sst')
+            location_info = self.complete_records_wo_sst[[self.unique_column_name, 'LATITUDE', 'LONGITUDE']]
+            df_reindexed = self.complete_records_wo_sst.set_index([self.unique_column_name, 'DATE'])
+
+        unique_locations = location_info.groupby(self.unique_column_name).first().reset_index().copy()
 
         temp1 = df_reindexed.unstack()[vars_to_retain].values
         temp1_index = df_reindexed.unstack()[vars_to_retain].index
-        output = pd.DataFrame(temp1, columns = column_name, index = temp1_index).reset_index()
 
-        output.set_index('STATION', inplace = True)
-        unique_locations.set_index('STATION', inplace = True)
+        output = pd.DataFrame(temp1, columns = column_name, index = temp1_index).reset_index()
+        output.set_index(self.unique_column_name, inplace = True)
+        unique_locations.set_index(self.unique_column_name, inplace = True)
 
         output_with_lat_lon = output.merge(unique_locations, left_index = True, right_index = True).reset_index()
         return output_with_lat_lon
 
 if __name__ == '__main__':
 
-    target_dir = './data/monthly_rainfall.csv'
+    # target_dir = './data/monthly_rainfall.csv'
     nc_mask_dir = '/Users/haigangliu/Dropbox/DataRepository/data_file_new/lsmask.nc'
     nc_data_dir = '/Users/haigangliu/Dropbox/DissertationCode/sst/sst.mnmean.nc'
 
-    save_dir = '/Users/haigangliu/Dropbox/DissertationCode/synthetic_data/with_sst_1_year.csv'
-    save_dir2 = '/Users/haigangliu/Dropbox/DissertationCode/synthetic_data/with_sst_1_year_flat_and_wide.csv'
+    # save_dir = './data/with_sst_1_year.csv'
+    # save_dir2 = '/Users/haigangliu/Dropbox/DissertationCode/synthetic_data/with_sst_1_year_flat_and_wide.csv'
 
-    monthly_rain = pd.read_csv(target_dir)
+    # monthly_rain = pd.read_csv(target_dir)
 
-    selector = RangeSelector(monthly_rain, '2015-01', '2015-12')
-    merged_table = selector.merge_with_sst(nc_data_dir, nc_mask_dir)
-    merged_table_flat_and_wide = selector.flip_to_flat_and_wide()
-    print(merged_table_flat_and_wide.head())
-    merged_table.to_csv(save_dir)
-    merged_table_flat_and_wide.to_csv(save_dir2)
+    # selector = RangeSelector(monthly_rain, '2015-01', '2015-12')
+    # merged_table = selector.merge_with_sst(nc_data_dir, nc_mask_dir)
+    # merged_table_flat_and_wide = selector.flip_to_flat_and_wide()
+    # print(merged_table_flat_and_wide.head())
+    # merged_table.to_csv(save_dir)
+    # merged_table_flat_and_wide.to_csv(save_dir2)
+
+    # ---- working with flood data ------
+
+    target_dir_flood = './data/flood_data_5_years.csv'
+    monthly_flood = pd.read_csv(target_dir_flood)
+
+    five_year =  RangeSelector(monthly_flood, '2011-01', '2015-12', unique_column_name = 'SITENUMBER', response_var = 'GAGE_MAX').flip_to_flat_and_wide(vars_to_retain = ['GAGE_MAX'])
+    five_year.to_csv('./data/flood_5_years_flat_and_wide.csv')
+
+    one_year =  RangeSelector(monthly_flood, '2015-01', '2015-12', unique_column_name = 'SITENUMBER', response_var = 'GAGE_MAX').flip_to_flat_and_wide(vars_to_retain = ['GAGE_MAX'])
+    one_year.to_csv('./data/flood_1_year_flat_and_wide.csv')
+
+
+
