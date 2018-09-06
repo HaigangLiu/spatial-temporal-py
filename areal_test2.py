d7072777b4fa42893db7fb7cdffc575819e9540f
Haigang Liu <haigang@email.sc.edu>
2018-09-06 14:22:55 -0400

    docstring and func name

--- areal_test2.py
+++ areal_test2.py
@@ -1,10 +1,10 @@
 import os
-from SampleDataLoader import load_flood_data
+from SampleDataLoader import load_flood_data, load_rainfall_data
 import geopandas as gpd
 import numpy as np
 from shapely.geometry import Point
 
-def water_shed_finder(data_frame, watershed_file=None, remove_singular=True):
+def water_shed_lookup(data_frame, watershed_file=None, remove_singular=True, key='STATION'):
     '''
     for each location, find which watershed it belongs to
     Args:
@@ -40,12 +40,24 @@ def water_shed_finder(data_frame, watershed_file=None, remove_singular=True):
     df['WATERSHED'] = names
 
     if remove_singular:
-        number_of_obs = df.groupby(['WATERSHED']).count()['SITENUMBER']
+        number_of_obs = df.groupby(['WATERSHED']).count()[key]
         singular_huc_areas = number_of_obs[number_of_obs==1].index
         df = df[~df.WATERSHED.isin(singular_huc_areas)]
     return df
 
-def neighbor_finder(dataframe, column):
+def generate_adjacent_matrix(dataframe, column):
+    '''
+    find the adjacent matrix and weight matrix for each location
+    based on the dummy variable of region
+    args:
+        dataframe (pandas dataframe): a dataframe with a column of regional information
+        column (string): the name of dummy variable that contains regional info
+    return:
+        three lists:
+            neighbor_matrix: an n by n matrix with binaries, 0 means not adjacent, 1 otherwise
+            adjacent_matrix, index for neighbors. [2, 4] means the touch border with 3rd and 5th observation
+            weight_matrix: replace all elements adjacent matrix with 1.
+    '''
     watershed_list = dataframe[column]
     neighbor_matrix = [] # an n by n matrix, 1 for adjcent, 0 for not.
     for entry in watershed_list:
@@ -53,19 +65,18 @@ def neighbor_finder(dataframe, column):
         neighbor_matrix.append(w)
     neighbor_matrix = np.array(neighbor_matrix)
 
-    adjcent_matrix = [] # a list of neighbors for each loc
+    adjacent_matrix = [] # a list of neighbors for each loc
     weight_matrix = []
     for idx, row in enumerate(neighbor_matrix):
         mask = np.argwhere(row == 1).ravel().tolist()
         mask.remove(idx) #delete the location itself.
-        adjcent_matrix.append(mask)
+        adjacent_matrix.append(mask)
         weight_matrix.append([1]*len(mask))
 
-    return neighbor_matrix, adjcent_matrix, weight_matrix
+    return neighbor_matrix, adjacent_matrix, weight_matrix
 
 
 def matrix_padding(input_list, max_padding=None):
-
     '''
     construct the matrix of W by making sure that every
     row has the same length.
@@ -88,12 +99,16 @@ def matrix_padding(input_list, max_padding=None):
 
     return np.array(input_list_copy)
 
-
 if __name__ == '__main__':
-    daily_flood = load_flood_data(option = 'daily')#usage
-    df_with_watershed_assigned = water_shed_finder(daily_flood)
-    print(df_with_watershed_assigned)
-    NM, AM, WM = neighbor_finder(df_with_watershed_assigned, 'WATERSHED')
+    daily_flood = load_flood_data(option='daily')
+    flood_with_watershed = water_shed_lookup(daily_flood, key='SITENUMBER')
+    f_NM, f_AM, f_WM = generate_adjacent_matrix(flood_with_watershed, 'WATERSHED')
+    padded_weights_f = matrix_padding(f_AM)
+    padded_adj_f = matrix_padding(f_WM)
 
-    padded_weights = matrix_padding(AM)
-    padded_adj = matrix_padding(WM)
+    daily_rain = load_rainfall_data(option='daily')
+    rain_with_watershed = water_shed_lookup(daily_rain, key='STATION')
+    r_NM, r_AM, r_WM = generate_adjacent_matrix(rain_with_watershed, 'WATERSHED')
+    padded_weights_r = matrix_padding(r_AM)
+    padded_adj_r = matrix_padding(r_WM)
+
