062e9a6930c733dad1828f894a14d20a1f1d08cd
Haigang Liu <haigang@email.sc.edu>
2018-09-11 12:54:06 -0400

    added preprocessing function

--- nws_datadownload.py
+++ nws_datadownload.py
@@ -1,12 +1,10 @@
-import os
-import re
-import requests
-
+import os, re, tarfile, requests
 from datetime import date, timedelta
 import concurrent.futures
-import tarfile
-#example_link
-#https://water.weather.gov/precip/archive/2014/01/01/nws_precip_1day_observed_shape_20140101.tar.gz
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+from shapely.geometry import Point
 
 class nwsDataDownloader:
     '''
@@ -16,12 +14,21 @@ class nwsDataDownloader:
         local_loc (string): the local dir to store data
         start (string): starting date: e.g. '1990-01-01'
         end (string): ending date: e.g. '1990-01-30'
+        rm_na (bool): if true, all locations with missing data will be removed
+        var_name (str): the name of the variable of interest.
+        region (polygon file, optional): the polygon file specifes the area of interest
+            default value is south carolina
+        to_pd (boolean): if true, the data will be converted to pandas dataframe before output.
     '''
-    def __init__(self, local_loc, start, end):
+    def __init__(self, local_loc, start, end, rm_na=True, var_name='GLOBVALUE', region=None, to_pd=True):
         self.web_loc = 'https://water.weather.gov/precip/archive'
         self.local_loc = local_loc
         self.start = start
         self.end = end
+        self.rm_na = rm_na #keep missing value or not
+        self.var_name = var_name #response variable name
+        self.region = region # the region user is interested in
+        self.to_pd = to_pd #output a pandas dataframe
 
     @staticmethod
     def range_handler(start_date, end_date):
@@ -49,11 +56,13 @@ class nwsDataDownloader:
     @staticmethod
     def _make_link(web_repo_loc, local_loc, date_token):
         '''
-        make the link, and the dir to store the file locally
+        generate the url for data, and local dir to store the data.
         Args:
             web_repo_loc (string): the link for the web archive with rainfall data
             local_loc (string): the dir to store data
             date_token (string): the date of data user wish to retrieve
+        Return
+            the url and the local dir.
         '''
         year_, month_, date_ = date_token.split('-')
         date_nospace = ''.join([year_, month_, date_])
@@ -66,8 +75,42 @@ class nwsDataDownloader:
 
         return link_in, dir_out
 
-    @staticmethod
-    def file_download_and_upack(in_and_out):
+    def post_process(self, shp_file):
+
+        region_mask = []
+
+        if self.region is None:
+            print('No polygon file specified.')
+            print('by default, all locations within south carolina will be retained')
+
+            sc_dir = os.path.join(os.getcwd(),'data/shape_file/south_carolina/tl_2010_45_state10.shp')
+            self.region = gpd.read_file(sc_dir)['geometry'][0]
+
+        geopandas_file = gpd.read_file(shp_file)
+
+        if self.rm_na: #remove na and negative values
+            before = geopandas_file.shape[0]
+            na_mask = np.isnan(geopandas_file[self.var_name])
+
+            geopandas_file = geopandas_file[~na_mask]
+            geopandas_file = geopandas_file[geopandas_file[self.var_name]>=0]
+            after = geopandas_file.shape[0]
+
+            print(f'removed missing data. {before-after} mssing values are removed')
+
+        locs = geopandas_file[['LON', 'LAT']]
+        for _, spatial_point in locs.iterrows():
+            region_mask.append(Point(spatial_point).within(self.region))
+
+        print(f'find {sum(region_mask)} values within in the specified region')
+        output = geopandas_file[region_mask]
+
+        if self.to_pd:
+            numpy_array = output[['LAT', 'LON', self.var_name]].values
+            output = pd.DataFrame(numpy_array, columns=['LATITUDE', 'LONGITUDE', 'PRCP'])
+        return output
+
+    def file_download_and_upack(self, in_and_out):
 
         input_link, name_out = in_and_out
         content = requests.get(input_link)
@@ -88,6 +131,15 @@ class nwsDataDownloader:
                 tar.extractall(os.path.join(dir_, target_folder_name))
 
             os.remove(name_out) #tear down
+
+            abs_target_folder = os.path.join(dir_, target_folder_name)
+            for file in os.listdir(abs_target_folder):
+                if file.endswith('.shp'):
+                    file_abs = os.path.join(abs_target_folder, file)
+                    output_from_post_process = self.post_process(file_abs)
+
+                    csv_name = abs_target_folder + '.csv'
+                    output_from_post_process.to_csv(csv_name)
             return 0
         else:
             raise ValueError('the file is empty. Check the link')
@@ -102,15 +154,12 @@ class nwsDataDownloader:
             in_and_outs.append([link_in, dir_out])
 
         executor = concurrent.futures.ProcessPoolExecutor(max_workers = 8)
-        start_downloading = executor.map(nwsDataDownloader.file_download_and_upack, in_and_outs)
-
-
-
+        start_downloading = executor.map(self.file_download_and_upack, in_and_outs)
 
 if __name__ == '__main__':
+    #example_link
+    #https://water.weather.gov/precip/archive/2014/01/01/nws_precip_1day_observed_shape_20140101.tar.gz
 
     local_loc_ = '/Users/haigangliu/SpatialTemporalBayes/rainfall_data_nc'
     from_date = '2015-06-01'; to_date = '2016-06-01'
-
     download_handler = nwsDataDownloader(local_loc_, from_date, to_date).run()
-
