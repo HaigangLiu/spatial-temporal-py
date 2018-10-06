1ed1c5e30194dcddaeb9a2bda01e62dffaf22d3e
Haigang Liu <haigang@email.sc.edu>
2018-10-06 10:03:18 -0400

    improved the perf of get_state_grid_points

--- utility_functions.py
+++ utility_functions.py
@@ -1,12 +1,10 @@
+import os, json, fiona, rasterio
 import numpy as np
 import pandas as pd
-import os, json
 from datetime import date, timedelta
-# from shapely.geometry import Point
-# from shapely.prepared import prep
 import geopandas as gpd
-import pandas as pd
-import rasterio
+from shapely.geometry import Point
+from shapely.prepared import prep
 
 def coordinates_converter(lat_lon_df, R = 3959):
     """
@@ -33,7 +31,7 @@ def coordinates_converter(lat_lon_df, R = 3959):
         raise TypeError('the only accepted input type is pandas dataframe')
         return None
 
-def generate_in_between_dates(start_date, end_date):
+def get_in_between_dates(start_date, end_date):
     '''
     for a start date and end date, generate the dates in between
     both start and end dates will be included in the final output
@@ -54,9 +52,11 @@ def generate_in_between_dates(start_date, end_date):
         list_of_dates.append(date_)
     return list_of_dates
 
-def get_state_range(state_name):
+def get_state_rectangle(state_name):
     '''
-    find maximum and miminum of latitude and longitude for a given state
+    find maximum and miminum of latitude and longitude for a given state,
+    a rectangle covers the state, in other words
+
     args:
         state_name: The name of the state. Can be full name or acrynom
             e.g.: either SC or South Carolina works.
@@ -79,7 +79,26 @@ def get_state_range(state_name):
             return None
     return data[state_name]
 
-def get_state_contours(state_name='South Carolina', use_cache=True):
+def get_state_contours(state_name):
+    '''
+    get the contour information for the give state name
+    '''
+    if len(state_name) == 2:
+        state_name = get_state_rectangle(state_name)['name']
+        print(state_name)
+    shape_for_states = gpd.read_file('./state_shapes/cb_2017_us_state_500k.shp')
+    # print(shape_for_states.NAME)
+    state_contour = shape_for_states[shape_for_states.NAME == state_name]
+
+    if len(state_contour):
+        state_contour = state_contour['geometry'].values[0]
+    else:
+        print(f'There is no contour information for {state_name}')
+        return None
+    return state_contour
+
+
+def get_state_grid_points(state_name='South Carolina', use_cache=True):
     '''
     find the polygon of a given state in the united states,
     also generate the dataframe file with all available locations for a certain state
@@ -93,95 +112,116 @@ def get_state_contours(state_name='South Carolina', use_cache=True):
         Can be full name or acrynom. e.g.: either SC or South Carolina works.
         use_cache (boolean)): will use the cached csv file to speed up the computation if possible
     '''
-    state_information = get_state_range(state_name) #use full name thereafter
+    state_information = get_state_rectangle(state_name) #use full name thereafter
     state_name = state_information['name']
 
-    shape_for_states = gpd.read_file('./state_shapes/cb_2017_us_state_500k.shp')
-    state_contour = shape_for_states[shape_for_states.NAME == state_name]
-
-    if len(state_contour):
-        state_contour = state_contour['geometry'].values[0]
-    else:
-        print(f'There is no contour information for {state_name}')
-        return None, None
+    state_contour = get_state_contours(state_name)
+    state_contour_ = prep(state_contour)
 
     if use_cache:
         try:
-            file_name = os.path.join('./state_shapes/', state_name + '.csv')
+            file_name = os.path.join('./state_shapes/', '.'.join([state_name, 'csv']))
             state_locations = pd.read_csv(file_name)
             print(f'loaded cached file for {state_name}')
-            return state_locations, state_contour
+            return state_locations
 
         except FileNotFoundError:
             print('cached file not found. start downloading from national weather service site.')
 
-    nws_locations = gpd.read_file('./all_locations/nws_precip_allpoint/nws_precip_allpoint.shp')
+    src_file = './all_locations/nws_precip_allpoint/nws_precip_allpoint.shp'
+    #prescreen info
+    min_lat = state_information["min_lat"]
+    max_lat = state_information['max_lat']
+    min_lng = state_information['min_lng']
+    max_lng = state_information['max_lng']
 
-    m1 = nws_locations.LAT > state_information["min_lat"]
-    m2 = nws_locations.LAT < state_information['max_lat']
-    m3 = nws_locations.LON > state_information['min_lng']
-    m4 = nws_locations.LON < state_information['max_lng']
-    pre_screened_points = nws_locations[m1 & m2 & m3 & m4]
+    file_name = '.'.join([state_name, 'csv'])
+    abs_file_dir = os.path.join('./data/', file_name)
 
-    state_contour_ = prep(state_contour)
-    domain = []
-    for idx, row in pre_screened_points.iterrows():
-        lon, lat = row[['LON', 'LAT']]
-        if state_contour_.contains(Point([lon, lat])):
-            domain.append(True)
-        else:
-            domain.append(False)
+    with fiona.open(src_file) as geo_file:
+        with open(abs_file_dir, 'w') as local_save:
+            local_save.write('LONGITUDE\tLATITUDE\n')
+            for line in geo_file:
+                lon, lat = line['geometry']['coordinates']
+                if (lat>max_lat) or (lat<min_lat) or (lon<min_lng) or (lon>max_lng):
+                    pass
+                else:
+                    if state_contour_.contains(Point([lon, lat])):
+                        local_save.write('\t'.join([str(lon), str(lat)]))
+                        local_save.write('\n')
+    print(f'the grid information has been saved to {abs_file_dir}')
+    return abs_file_dir
 
-    in_domain_points = pre_screened_points[domain]
-    if len(in_domain_points) > 0:
-        state_locations = in_domain_points[['LAT', 'LON']].round(4)
-    else:
-        print(f'no observational locations found in {state_name}')
-        return None, None
-    return state_locations, state_contour
+def get_elevation(input, key=None, lat='LATITUDE', lon='LONGITUDE'):
+    '''
+    find the elevation for given latitude and longitude
+    the input can be of list like [lon, lat]
+    or a dataframe that contains longitude column and latitude column
 
-
-def find_elevation(dataframe_copy, key, lat='LATITUDE', lon='LONGITUDE'):
-
+    Args:
+        input (pandas dataframe, or list)
+        key: if dataframe is given, key must be provided to indicate which
+        column is station name
+    Return:
+        if input is a list:
+            returns the height as a float number
+        if input is dataframe:
+            returns the original dataframe within an additional column for
+            elevation
+    '''
     source_1 = './data/shape_file/elevation_info/srtm_20_05/srtm_20_05.tif'
     source_2 = './data/shape_file/elevation_info/srtm_20_06/srtm_20_06.tif'
     source_3 = './data/shape_file/elevation_info/srtm_21_05/srtm_21_05.tif'
     source_4 = './data/shape_file/elevation_info/srtm_21_06/srtm_21_06.tif'
-
-    lats_and_lons = dataframe_copy.groupby([key]).first()[[lat,lon]]
     source_collection = [source_1, source_2, source_3, source_4]
 
-    for idx, lat_and_lon in lats_and_lons.iterrows():
-
-        lat_and_lon = list(lat_and_lon)
-        mask = dataframe_copy[key] == idx
-
+    def get_elevation_one_loc(input):
+        elevation = None
         for source in source_collection:
             with rasterio.open(source) as f:
                 try:
-                    vals = f.sample([lat_and_lon])
+                    vals = f.sample([input])
                     for val in vals:
                         if val >= 0: #lowest point of sc is sea level
-                            dataframe_copy.loc[mask, 'ELEVATION'] = val[0]
+                            elevation = val[0]
                 except IndexError:
                     continue
                 else:
                     break
-    return dataframe_copy
+        return elevation
+
+    if isinstance(input, list):
+        print('assuming the format is [longitude, latitude]')
+        print('the unit is measured in meter')
+        return get_elevation_one_loc(input)
+
+    elif isinstance(input, pd.DataFrame):
+        if key is None:
+            raise ValueError('Need to specify the name of a column of station identifier. e.g. STATIONID ')
+        try:
+            #construct a summary table to enhance performance
+            summary = input.groupby([key]).first().reset_index()[[key, lon, lat]]
+
+            input.set_index(key, inplace=True)
+            for idx, row in summary.iterrows():
+                key, lat, lon = row.values
+                height = get_elevation_one_loc([lat, lon])
+                input.loc[key, 'ELEVATION'] = height
+
+        except KeyError:
+            print(f'make sure the dataframe contains: LATITUDE, LONGITUDE and {key}')
+            return None
+    output = input.reset_index()
+    return output
 
 if __name__ == '__main__':
 
-    jsonfile = open('./state_shapes/state_boundaries.json')
-    data = json.load(jsonfile)
+    a = get_state_contours('Wisconsin')
+    b = get_state_grid_points('Wisconsin')
+    c = get_state_rectangle('Wisconsin')
+    d = get_elevation([33, -81])
+    e = get_in_between_dates('1990-01-01','1990-01-04' )
+    print(e)
 
-    a, b = get_state_contours('VI') #virgin island
-    c, d = get_state_contours('south carolina') #now allow lower case
 
-    for state in data.keys(): #for all states
-        result = get_state_contours(state)
-        if result:
-            abs_dir = os.path.join('./state_shapes', data[state]['name']+'.csv')
-            locs, _ = result
-            locs.to_csv(abs_dir)
-            print(f'finished processing state {state}')
 
