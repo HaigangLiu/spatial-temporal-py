a40a06ec691847657fdcd70ecf4ededf6dce046c
Haigang Liu <haigang@email.sc.edu>
2018-10-12 09:42:48 -0400

    added doctring

--- merge_rain_and_flood.py
+++ merge_rain_and_flood.py
@@ -1,71 +1,57 @@
-import os
+from multiprocessing import Pool, cpu_count
 import pandas as pd
 from scipy import spatial
+from utility_functions import get_in_between_dates
 
-def location_based_matcher(main_df, query_df, varname):
+class Merger:
     '''
-    Used to merge two data files with different coordinates
+    A class to merge two variables based on two data sources.
+    For instance, there is rainfall data from charleston, but we only
+    got gage data from columbia. Hence, we intend to merge the auxillary
+    variable to the main dataframe.
 
-    The main file is the original file, and the query df is
-    the dataframe from where we borrow information.
-
-    Example use case: the main_df is flood  while query df is about rain
-    this function can help finding the nearest rain info for each observation
-    in flood file and generate an addition column
+    User decides which dataframe is the main one and which one is for query purpose, and can specify the start and the end date of the merging operation.
     '''
-    main_df_copy = main_df.copy()
 
-    latitude = query_df.LATITUDE.values
-    longitude = query_df.LONGITUDE.values
-    kdtree = spatial.KDTree(list(zip(latitude, longitude)))
 
-    #items to be queried
-    pts_lat = main_df.LATITUDE.values
-    pts_lon = main_df.LONGITUDE.values
-    pts = list(zip(pts_lat, pts_lon))
+    def __init__(self,  main_df, query_df, start, end, varname='PRCP'):
+        self.main_df = main_df
+        self.query_df = query_df
+        self.list_of_dates = get_in_between_dates(start, end)
+        self.varname = varname
 
-    _, idx  = kdtree.query(pts, k=1)
-    values_from_neighors = query_df.iloc[idx][varname].values
-    main_df_copy[varname] = values_from_neighors
+    def location_match_single_day(self, main_df, query_df):
+        main_df_copy = main_df.copy()
+        latitude = query_df.LATITUDE.values
+        longitude = query_df.LONGITUDE.values
+        kdtree = spatial.KDTree(list(zip(latitude, longitude)))
 
-    return main_df_copy
+        #items to be queried
+        pts_lat = main_df.LATITUDE.values
+        pts_lon = main_df.LONGITUDE.values
+        pts = list(zip(pts_lat, pts_lon))
 
-def flood_and_rain_merge(main_df, start, end):
-    '''
-    merge the dataframe of different variables.
-    the main dataframe contains, e.g. gage information and locations
-    and for each location, we find the nearest rainfall information based on another dataframe.
+        _, idx  = kdtree.query(pts, k=1)
+        values_from_neighors = query_df.iloc[idx][self.varname].values
+        main_df_copy[self.varname] = values_from_neighors
+        return main_df_copy
 
-    Args
-    main_df (pandas dataframe): dataframe with location information indexed by latitude and longitude
-    start (string): the start date. format should be 1990-09-09
-    end (end): the end date. the format should be the same with start
-    '''
-    main_df_copy = main_df.copy()
-    timestamps = pd.to_datetime(main_df_copy.DATE)
-    start, end = pd.to_datetime(start), pd.to_datetime(end)
-    boolean_mask = (timestamps > start) & (timestamps < end)
-    df_selected = main_df_copy[boolean_mask]
+    def select_date(self, date_):
+        flood_info = self.main_df[self.main_df.DATE == date_]
+        rainfall_info = self.query_df[self.query_df.DATE == date_]
+        return self.location_match_single_day(flood_info, rainfall_info)
 
-    container = []
-    for date in pd.unique(df_selected.DATE):
-
-        df_one_day = df_selected[df_selected.DATE == date]
-        daily_prcp_file_name = ''.join(date.split('-')) + '.txt'
-        file_dir = os.path.join('./rainfall_nws/', daily_prcp_file_name)
-
-        try:
-            prcp_for_that_day = pd.read_csv(file_dir, sep=' ')
-            merged_rain_and_flood = location_based_matcher(df_one_day, prcp_for_that_day, varname='PRCP')
-            print(f'finshed prcessoing for the data in {date}')
-            container.append(merged_rain_and_flood)
-
-        except FileNotFoundError:
-            print(f'warning: the file for {date} is missing. Consider downloading it again')
-    return pd.concat(container, ignore_index=True)
+    def run(self):
+        pool = Pool(processes=cpu_count())
+        result = pool.map(self.select_date, self.list_of_dates)
+        return pd.concat(list(result))
 
 if __name__ == '__main__':
-    #example
-    base_file_for_gage = pd.read_csv('./data/flood_data_daily_beta.csv', index_col= 0)
-    data = flood_and_rain_merge(base_file_for_gage, '2009-01-01', '2009-06-10')
-    print(data.head())
+    import pickle
+    with open('./data/cached_combined_file.pickle', 'rb') as pickle_file:
+        rain = pickle.load(pickle_file)
+    flood = pd.read_csv('./data/flood_data_daily_beta.csv', index_col=0, dtype={'SITENUMBER': str})
+
+    hanlder = Merger(flood, rain, '2007-01-01', '2017-12-31')
+    merged_dataset = hanlder.run()
+    merged_dataset.to_csv('./data/rainfall_and_flood_11.csv')
