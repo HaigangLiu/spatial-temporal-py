8641f83ddd678c6b284f067ee74ad549730d6fee
Haigang Liu <haigang@email.sc.edu>
2018-11-22 15:48:07 -0500

    optimized the logic of file formatter

--- downloader_beta.py
+++ downloader_beta.py
@@ -1,16 +1,14 @@
-import re, os, requests
-from bs4 import BeautifulSoup
-from utility_functions import get_state_fullname
+import os
+import requests
 import pandas as pd
 from shutil import rmtree
+from utility_functions import get_state_fullname
 
 ATTRIBUTE_NUMBER = {'GAGE': '00065'}  #gage level
-STAT_IDENTIFIER = {'MAX': '00001'} #max, min, mean
-#maybe trying downloading rainfall
-#add a clean up step
-class DailyFloodDataDownloader:
-    def __init__(self, start, end, state, short_version=True, cap=None, verbose=False):
+STAT_IDENTIFIER = {'MAX': '00001', 'MIN':'00002','MEAN':'00003'} #max, min, mean
 
+class DailyFloodDataDownloader:
+    def __init__(self, start, end, state, eight_digit_station_only=True, cap=None, verbose=False):
         '''
         download flood data for given state within a given time frame
         1. first go to nws website to grab a list of available stations in that area
@@ -23,7 +21,7 @@ class DailyFloodDataDownloader:
             end (string): the end time, format: '1990-01-01'
             state (string): the name or acrynom for that state
                 all of these will work: 'SC', 'South Carolina', 'sc'
-            short_version (boolean): if true, only use stream station and discard other stations which might be irrelelavent
+            eight_digit_station_only (boolean): if true, only use stream station and discard other stations which might be irrelelavent
             verbose (boolean): if true, more information will be given when downloading. Otherwise only progress will be reported.
             cap (int) for demo purpose, we can limit the number of locations
         Example:
@@ -35,7 +33,7 @@ class DailyFloodDataDownloader:
         self.verbose =verbose
         self.cap = cap
         self.job_list = []
-        self.save_file_name = self._make_filename(start=start, end=end, state=self.state)
+        self.filename = self._make_filename(start=start, end=end, state=self.state)
 
         var_code = ATTRIBUTE_NUMBER['GAGE']
         _url_part_1 = f'https://waterdata.usgs.gov/nwis/dv?cb_{var_code}=on&format=rdb&site_no='
@@ -43,14 +41,14 @@ class DailyFloodDataDownloader:
 
         print('-'*40)
         print(f'start looking for all available stations in {state}')
-        if short_version:
+        if eight_digit_station_only:
             print('only 8-digit station will be kept since they are more likely to be relevant')
-            print('to turn off this option, set short_version=False')
+            print('to turn off this option, set eight_digit_station_only=False')
         else:
             print('all locations, including wells and glaciers, will be examined.')
             print('this might make the process longer.')
 
-        self.station_info_dict = self._get_stations(self.state, short_version=True)
+        self.station_info_dict = self._get_stations(self.state, eight_digit_station_only=True)
         self.stations = list(self.station_info_dict.keys())
 
         print('finished generating locations, now start downloading files.')
@@ -74,7 +72,7 @@ class DailyFloodDataDownloader:
         name = '-'.join([state, start_, end_])
         return name + '.txt'
 
-    def _get_stations(self, state, short_version):
+    def _get_stations(self, state, eight_digit_station_only):
         if len(state) > 2:
                 state = get_state_fullname(state, reverse=True)#get acrynom
         state = state.lower()
@@ -87,7 +85,7 @@ class DailyFloodDataDownloader:
                 siteid, name, latitude, longitude = line.split('\t')[1:5]
                 dict_of_stations[siteid] = (name, latitude, longitude)
 
-        if short_version:
+        if eight_digit_station_only:
             shorter_dict = {}
             for k, v in dict_of_stations.items():
                 if len(k) == 8:
@@ -97,14 +95,18 @@ class DailyFloodDataDownloader:
             return shorter_dict
         return dict_of_stations
 
-    def page_parser(self, station_url, stationid):
+    def page_parser(self, station_url, additional_info, outfilename):
         '''
-        Args: the station id (str)
-        return: a dataframe of gage informaion
-        '''
+        parse the url and save the result in a txt file
+        allow users to add additional information like latitude and longitude
+        the additional information has to be a dict
 
-        name, latitude, longitude = self.station_info_dict[stationid]
-        stationid = str(stationid)
+        Note that if the file has 2 lines, the additional info will be
+        appended to each of them.
+        '''
+        keywords = '\t'.join(list(additional_info.keys()))
+        values = '\t'.join(list(additional_info.values()))
+
         content = requests.get(station_url).text
         obs = []; header = None
 
@@ -112,7 +114,7 @@ class DailyFloodDataDownloader:
             print(f'Got an empty page')
             return None
 
-        textfile_name = '.'.join([stationid, 'txt'])
+        textfile_name = '.'.join([outfilename, 'txt'])
         textfile_name = os.path.join(self.temp_dir, textfile_name)
 
         with open(textfile_name, 'w') as f:
@@ -121,12 +123,11 @@ class DailyFloodDataDownloader:
                 is_var_name = line.startswith('agency_cd')
 
                 if is_var_name:
-                    header = line
-                    header = '\t'.join(['name', 'latitude', 'longitude', header])
-                    f.write(header+'\n')
+                    line = '\t'.join([keywords, line])
+                    f.write(line+'\n')
 
                 if is_obs:
-                    line = '\t'.join([name, latitude, longitude, line])
+                    line = '\t'.join([values, line])
                     f.write(line+'\n')
 
         if os.path.getsize(textfile_name):
@@ -150,39 +151,49 @@ class DailyFloodDataDownloader:
                 look_up_table[new_key] = new_v
 
         header = {'SITENUMBER': 'site_no',
-                  'STATIONNAME': 'name'
+                  'STATIONNAME': 'name',
                   'LATITUDE':'latitude',
                   'LONGITUDE': 'longitude',
                   'DATE':'datetime',
                   }
-
+                #'gage_max': '--006450--001'
         header.update(look_up_table)
         return header
 
     def file_formatter(self, filename, format_dict):
         '''
+        format_dict gives the instruction of how to rename file
         renaming several variables and remove duplicate columns
         '''
         look_up_table = format_dict
-
+        reversed_table = {v: k for k, v in look_up_table.items()}
         dataframe = pd.read_csv(filename, sep='\t', dtype={'site_no':str})
-        for colname, colcode in look_up_table.items():
-            for col in dataframe.columns:
-                if col.endswith(colcode) and colname not in dataframe.columns:
-                    dataframe.rename(columns={col: colname}, inplace=True)
-                elif col.endswith(colcode) and colname in dataframe.columns:
-                    print(f'source file has duplicate columns of {colname}')
-                elif col.endswith('_cd'):
-                    dataframe.drop([col], axis=1, inplace=True)
-                else:
-                    pass
+
+        for col in dataframe.columns:
+            if not col.endswith(tuple(list(look_up_table.values()))):
+                dataframe.drop([col], axis=1, inplace=True)
+            else:
+                try:
+                    new_name = reversed_table[col]
+                except KeyError:
+                    list_of_keys = list(reversed_table.keys())
+                    right_key = list(filter(col.endswith, list_of_keys))[0]
+                    new_name = reversed_table[right_key]
+
+                dataframe.rename(columns={col: new_name}, inplace=True)
         dataframe.to_csv(filename, index=False)
-        #index will be detemined after combining all small files
 
     def run(self):
+
         valid_station = 0
+
         for idx, (stationurl, stationid) in enumerate(zip(self.job_list, self.stations)):
-            filename = self.page_parser(station_url=stationurl, stationid=stationid)
+            name, latitude, longitude = self.station_info_dict[stationid]
+            additional_info_dict = {'name': name, 'latitude': latitude, 'longitude': longitude}
+            filename = self.page_parser(station_url=stationurl,
+                                        additional_info=additional_info_dict,
+                                        outfilename=stationid)
+
             if filename:
                 self.file_formatter(filename, self.colnames_table)
                 valid_station = valid_station + 1
@@ -198,7 +209,7 @@ class DailyFloodDataDownloader:
                 print(f'found {self.cap} stations already. teminating the program')
                 break
 
-        with open(self.save_file_name, 'w') as file:
+        with open(self.filename, 'w') as file:
             header = ','.join(list(self.colnames_table.keys())) + '\n'
             file.write(header)
             for small_file in os.listdir(self.temp_dir):
@@ -209,11 +220,11 @@ class DailyFloodDataDownloader:
                         file.write(sf.read())
 
         rmtree(self.temp_dir) #tear down
-        print(f'the data have have been saved into the file {self.save_file_name}.')
+        print(f'the data have have been saved into the file {self.filename}.')
         print(f'the location of file is {os.getcwd()}')
 
 if __name__ == '__main__':
-    test = DailyFloodDataDownloader(start='2010-01-01', end='2010-01-02', state='SC', cap=10, short_version=False)
+    test = DailyFloodDataDownloader(start='2010-01-01', end='2010-01-02', state='SC', cap=10, eight_digit_station_only=False)
     test.run()
 
     # https://waterdata.usgs.gov/nwis/dv?cb_00065=on&format=rdb&site_no=02153051&referred_module=sw&period=&begin_date=2017-11-20&end_date=2018-11-20
