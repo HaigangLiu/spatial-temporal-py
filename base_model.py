869b918211cafb6ec70641ab8bbb8f3bbf62788e
Haigang Liu <haigang@email.sc.edu>
2019-02-10 19:18:05 -0500

    minor changes

--- base_model.py
+++ base_model.py
@@ -4,21 +4,39 @@ import numpy as np
 class BaseModel:
     def __init__(self, response, locations, covariates=None):
 
-        self.response = response
-        self.locations = locations
-        self.covariates = covariates
-
-        if covariates is None: #no covariate information included
-            self.covariates = []
-        elif type(covariates) == list:
-            pass
+        for arg in [response, locations]:
+            if isinstance(arg, np.ndarray):
+                self.response = response
+                self.locations = locations
+            else:
+                raise TypeError('Need input to be ndarray, or use classmethod from_panadas()')
+        if covariates is None:
+            self.covariates = [np.ones_like(response)]
         else:
-            raise TypeError('covariates must be a list of numpy arrays')
-            return None
+            if isinstance(covariates, list):
+                if BaseModel.covariate_is_legit(covariates):
+                    covariates.insert(0, np.ones_like(response))
+                self.covariates = covariates
+            else:
+                raise TypeError('covariates can be only either None or a list of numpy arrays')
 
         self.predicted = None
         self.trace = None
         self.model = None
+
+    @staticmethod
+    def covariate_is_legit(list_of_covars):
+        '''
+        make sure each covariate comes with identical dimension
+        '''
+        shape_info = list_of_covars[0].shape
+        for covariate_arr in list_of_covars:
+            shape_info_temp = covariate_arr.shape
+            if shape_info == shape_info_temp:
+                pass
+            else:
+                raise TypeError('All covariates should have the same dimension')
+        return True
 
     @classmethod
     def from_panadas(cls, dataframe, response, locations, covariates=None):
@@ -33,51 +51,28 @@ class BaseModel:
             raise TypeError('the response variable has to be string of column name of repsonse varaible')
         if type(locations)!= list:
             raise TypeError('Locations has to be a list of column names that detemines the location')
-        if covariates:
-            if type(covariates)!= list:
-                raise TypeError('Covariates has to be a list of column names that detemines the covariates or simply NoneType')
+        if covariates and (type(covariates)!= list):
+            raise TypeError('Covariates must be a list or NoneType')
         try:
             reponse = dataframe[response].values
             locations = dataframe[locations].values
             if covariates:
-                covariates_numpy = []
+                cov_numpy_list = []
                 for covariate in covariates:
-                    covariates_numpy.append(dataframe[covariate].values)
+                    cov_numpy_list.append(dataframe[covariate].values)
             else:
-                covariates_numpy = None
+                cov_numpy_list = None
         except KeyError:
             print('make sure all keywords are in the columns of your dataframe')
             return None
-        return cls(reponse, locations, covariates_numpy)
+        return cls(reponse, locations, cov_numpy_list)
 
-    def get_metrics(self, sample_size=5000):
-
-        if self.predicted is None:
-            self._predict_in_sample(sample_size=sample_size, use_median=False)
-
-        waic = pm.waic(self.trace, self.model).WAIC
-        loo = pm.loo(self.trace, self.model).LOO
-        l1_loss = np.mean(np.abs(self.response - self.predicted))
-        l2_loss = np.mean(np.square(self.response - self.predicted))
-
-        table = [['Metrics', 'Value']]
-        metrics = []
-        metrics.append(['mse', str(round(l2_loss, 4))])
-        metrics.append(['mae', str(round(l1_loss, 4))])
-        metrics.append(['loo',str(round(loo, 4))])
-        metrics.append(['aic', str(round(waic, 4))])
-        table.extend(metrics)
-
-        header = 'Model Fitting Metrics Report'
-        BaseModel.pretty_print(header, table, table_len=50)
-
-    def sample(self, sample_size=5000, burn_in=1000, nchains=4):
+    def sample(self, sample_size=5000, burn_in=1000, nchains=1):
         if self.model is None:
             self.fit()
 
         with self.model:
-            start = pm.find_MAP()
-            self.trace = pm.sample(sample_size, tune=burn_in, nchains=nchains)
+            self.trace = pm.sample(sample_size, tune=burn_in)
 
     def fast_sample(self, sample_size=5000, iters=10000):
         if self.model is None:
@@ -93,9 +88,11 @@ class BaseModel:
         need to generate three attributes when inherited:
         self.trace, self.model, and self.predicted
         '''
-        raise NotImplementedError('this is the fit method base class. Should not be called in common use case.')
+        raise NotImplementedError('this is a base class. Should not be called in a normal use case.')
 
     def _predict_in_sample(self, sample_size, use_median):
+        if self.model is None:
+            self.fit()
 
         with self.model:
             simulated_values = pm.sample_ppc(self.trace)['Y']
@@ -105,12 +102,12 @@ class BaseModel:
             self.predicted = np.mean(simulated_values, axis=0)
         return self.predicted
 
-    def get_parameter_estimation(self, varnames, sig_level):
+    def get_param_estimates(self, varnames, sig_level):
 
         lower_threshold = (100 - sig_level*100)/2
         upper_threshold = 100 - lower_threshold
 
-        def make_conclusion(lower_bound, upper_bound):
+        def draw_conclusion(lower_bound, upper_bound):
             if upper_bound > 0 and lower_bound > 0:
                 return '***'
             elif upper_bound < 0 and lower_bound < 0:
@@ -139,9 +136,8 @@ class BaseModel:
                 packed_info = zip([means], [lower_bounds], [upper_bounds])
 
             for idx, entry in enumerate(packed_info):
-
                 mean, lower_bound, upper_bound = entry
-                conclusion = make_conclusion(lower_bound, upper_bound)
+                conclusion = draw_conclusion(lower_bound, upper_bound)
                 mean = str(round(mean, 4))
                 lower_bound = str(round(lower_bound, 4))
                 upper_bound = str(round(upper_bound, 4))
@@ -151,7 +147,6 @@ class BaseModel:
                 temp = temp[0]
                 temp.insert(0, varname)
                 output.append(temp)
-
             else:
                 for idx, row in enumerate(temp):
                     varname_ = '_'.join([varname, str(idx)])
@@ -159,7 +154,7 @@ class BaseModel:
                     output.append(row)
 
         header = 'Parameter Estimates'
-        BaseModel.pretty_print(table_to_print=output, header=header)
+        BaseModel.pretty_print(content=output, header=header)
         output_dict = {} # build a dict for easier info retrval
         for entry in output:
             key =  entry.pop(0)
@@ -167,17 +162,42 @@ class BaseModel:
             output_dict[key] = values
         return output_dict
 
+    def get_metrics(self, kind=['mse', 'mae', 'loo','aic'], sample_size=5000):
+        if self.predicted is None:
+            self._predict_in_sample(sample_size=sample_size, use_median=False)
+
+        records = {}
+        for kind_ in kind:
+            if kind_.lower() == 'mse':
+                records['mse'] = np.mean(np.square(self.response - self.predicted))
+            elif kind_.lower() == 'mae':
+                records['mae'] = np.mean(np.abs(self.response - self.predicted))
+            elif kind_.lower() == 'waic':
+                records['waic'] = pm.waic(self.trace, self.model).WAIC
+            elif kind_.lower() == 'loo':
+                records['loo'] = pm.loo(self.trace, self.model).LOO
+            else:
+                raise ValueError(f'{kind_} is not supported.')
+
+        table_content = [['Metrics', 'Value']]
+        for key, value in records.items():
+            value = str(round(value, 4))
+            table_content.append([key, value])
+
+        header = 'Model Fitting Metrics Report'
+        BaseModel.pretty_print(header, table, table_len=50)
+
     @staticmethod
-    def pretty_print(header, table_to_print, table_len=80):
+    def pretty_print(header, content, table_len=80):
         '''
         header (string): header of table
-        table_to_print (list of list): every list is a row.
+        content (list of list): every list is a row.
             list element has to be string
         '''
         print('-'*table_len)
         print(header)
-        col_width = max(len(word) for row in table_to_print for word in row) + 2  # padding
-        for row in table_to_print:
+        col_width = max(len(word) for row in content for word in row) + 2  # padding
+        for row in content:
             print('-'*table_len)
             print("".join(word.ljust(col_width) for word in row))
         print('-'*table_len)
